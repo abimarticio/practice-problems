680a99d6448ff98ff3fb9889f6423cf4d2071140
Abigail Marticio <marticiobgl@gmail.com>
2021-03-03 23:02:49 +0800

    chore: delete file

--- basic/prob_1.py
+++
@@ -1,8 +0,0 @@
-# Slice the following input: “hello” to get the following output: “hlo”
-
-hello = "hello"
-
-print(hello[::2])
-print(hello[:len(hello):2])
-print(hello[0::2])
-print(hello[0:len(hello):2])
