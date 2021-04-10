7ad47cf65d80c44fd071d9edfdd251e50a745bc5
Abigail Marticio <marticiobgl@gmail.com>
2021-04-10 21:11:36 +0800

    chore: add list_lines function

--- nlp/prob_02.py
+++ nlp/prob_02.py
@@ -3,7 +3,21 @@ def load_data(filename: str) -> list:
         data = text_file.readlines()
     return data
 
+def list_lines(data: list):
+    lines = []
+    for line in data:
+        line = line.strip('\n')
+        if ':' in line:
+            line = re.sub(r"[^a-zA-Z0-9:'[\](\)]+", " ", line)
+            line = re.sub(r'\[.*?\]', '', line)
+            line = re.sub(r'(?:^|\W)English(?:$|\W)\)|(?:^|\W)Translated(?:$|\W)|\((.*?)\)|(\))', '', line)
+            line = line.strip()
+            if line != "":
+                lines.append(line)
+    return lines
+
 def main():
     data = load_data(filename='avengers_endgame.txt')
+    lines = list_lines(data=data)
 
 main()
