9962c2c0c21ca3195fb8f4a25ac1c1e182fdc025
Abigail Marticio <marticiobgl@gmail.com>
2021-04-10 21:07:46 +0800

    chore: add main

--- nlp/prob_02.py
+++ nlp/prob_02.py
@@ -2,3 +2,8 @@ def load_data(filename: str) -> list:
     with open(filename, 'r', encoding='utf-8') as text_file:
         data = text_file.readlines()
     return data
+
+def main():
+    data = load_data(filename='avengers_endgame.txt')
+
+main()
